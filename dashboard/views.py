cc98abdd1429b8cd5917602b7109668e5f08ec06
Rohit <rohit.p120101@gmail.com>
2023-10-16 21:09:58 +0530

    Initial Commit

---
+++ dashboard/views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from item.models import Item
+
+def dashboard(request):
+    items = Item.objects.filter(created_by=request.user)
+    return render(request,'dashboard/index.html',{
+        'items':items
+    })
+
---
+++ item/models.py
@@ -0,0 +1,37 @@
+from collections.abc import Iterable
+from django.contrib.auth.models import  User
+from django.db import models
+from PIL import Image
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Item(models.Model):
+    category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self) -> str:
+        return self.name
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width >300:
+            img.thumbnail((300,300))
+            img.save(self.image.path)
